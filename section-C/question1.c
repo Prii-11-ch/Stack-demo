9c01a551653621ce350b315f61e6dcbdda1796c9
Prii-11-ch <chandelpishu@gmail.com>
2025-12-22 22:51:07 +0530

    question1.c

--- section-C/question1.c
+++ section-C/question1.c
@@ -1,19 +1,44 @@
 #include <stdio.h>
+#define MAX 10
 
-// Recursive function for Tower of Hanoi
-void toh(int n, char source, char aux, char dest) {
-    if (n == 1) {
-        printf("Move disk 1 from %c to %c\n", source, dest);
+int data[MAX];
+int nextNode[MAX];
+int head = -1;
+int freePos = 0;
+
+void insert(int value) {
+    if (freePos == MAX) {
+        printf("List is full\n");
         return;
     }
-    toh(n - 1, source, dest, aux);
-    printf("Move disk %d from %c to %c\n", n, source, dest);
-    toh(n - 1, aux, source, dest);
+
+    data[freePos] = value;
+    nextNode[freePos] = head;
+    head = freePos;
+    freePos++;
+}
+
+void display() {
+    int temp = head;
+    printf("Linked List: ");
+    while (temp != -1) {
+        printf("%d -> ", data[temp]);
+        temp = nextNode[temp];
+    }
+    printf("NULL\n");
 }
 
 int main() {
-    int n = 3;
-    printf("Steps to solve Tower of Hanoi for %d disks:\n", n);
-    toh(n, 'A', 'B', 'C');
+    insert(10);
+    insert(20);
+    insert(30);
+
+    display();
+
     return 0;
 }
+
+
+	
+
+
